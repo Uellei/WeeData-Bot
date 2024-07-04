b262f8b8d57a191f39e46484a90d101e5d0d3c18
Uellei <wessmarcelo@gmail.com>
2024-07-03 22:26:49 -0300

    Add Hello World endpoint for proxy

--- frontend/src/api/proxy.js
+++ frontend/src/api/proxy.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const request = require('request');
 const app = express();
 
-app.get('/proxy', (req, res) => {
-    const url = req.query.url;
-    request.get(url).pipe(res);
+app.get('/api/proxy', (req, res) => {
+    res.send('Hello World');
 });
 
-app.listen(3001, () => {
-    console.log('Proxy server running on port 3001');
-});
+module.exports = app;
